93328e2de7fc140a9b5e1fa6f879e54b8d4d0a7b
sshintaku <sshintaku07020@gmail.com>
2022-09-28 18:38:10 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/sshintaku/prisma_session"
+)
+
+func main() {
+	var params = prisma_session.ReadParameters()
+	session := prisma_session.Session{ApiUrl: params.ApiUrl}
+	session.CreateSession()
+	session.GetAuditEvents()
+
+}
